2e9b2c45b0b013f644f89f6974ecd7f72632fc40
packard_TM <a174126725@gmail.com>
2023-12-22 13:40:23 +0300

    add all

--- Lesson1/Task2/Program.cs
+++ Lesson1/Task2/Program.cs
@@ -1,26 +1,35 @@
-﻿int i1 = 5;
-int i2 = 6;
-int sum = i1 + i2;
-Console.WriteLine(sum);
+﻿// int i1 = 5;
+// int i2 = 6;
+// int sum = i1 + i2;
+// Console.WriteLine(sum);
 
-long l1 = 6;
-long sum2 = l1 + i1;
-Console.WriteLine(sum2);
+// long l1 = 6;
+// long sum2 = l1 + i1;
+// Console.WriteLine(sum2);
 
-double d1 = 5.5;
-double sum3 = d1 + i1;
-Console.WriteLine(sum3);
+// double d1 = 5.5;
+// double sum3 = d1 + i1;
+// Console.WriteLine(sum3);
 
-int product1 = i1 * i2;
-long product2 = l1 * i1;
-double product3 = d1 * i1;
-Console.WriteLine(product1);
-Console.WriteLine(product2);
-Console.WriteLine(product3);
+// int product1 = i1 * i2;
+// long product2 = l1 * i1;
+// double product3 = d1 * i1;
+// Console.WriteLine(product1);
+// Console.WriteLine(product2);
+// Console.WriteLine(product3);
 
-int quotient = i1 / i2;
-Console.WriteLine(quotient);
+// int quotient = i1 / i2;
+// Console.WriteLine(quotient);
 
-d1 = 6;
-double quotient2 = i1 / d1;
-Console.WriteLine(quotient2);
+// d1 = 6;
+// double quotient2 = i1 / d1;
+// Console.WriteLine(quotient2);
+
+// домашнее задание
+
+int a = 6;
+double b = 5.5;
+int c = 7;
+int d = 8;
+double value = a * b / (c + d);
+Console.WriteLine(value);
