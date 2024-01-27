e5687fa7ccbb181de4560cff3bd8db64907ab504
packard_TM <a174126725@gmail.com>
2024-01-27 13:31:44 +0300

    add lesson5

---
+++ Lesson5/Task0/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.Clear();
+
+void OpenMatreshka(int size)
+{
+    Console.WriteLine($"Открыта матрешка размера {size}...");
+    if (size == 1)
+    {
+        Console.WriteLine("Открыта самая маленькая матрешка!");
+        return;
+    }
+    OpenMatreshka(size-1);
+}
+OpenMatreshka(5);
+
+Console.WriteLine();
+
+void OpenMatreshka1(int size)
+{
+    if (size == 1)
+    {
+        Console.WriteLine("Самая Маленькая матрешка!");
+        return;
+    }
+    OpenMatreshka1(size-1);
+    Console.WriteLine($"Открыта матрешка размера {size}...");
+}
+OpenMatreshka1(5);
+
+Console.WriteLine();
+
+void OpenMatreshka2(int size)
+{
+    if (size == 1)
+    {
+        Console.WriteLine("Самая Маленькая матрешка!");
+        return;
+    }
+    Console.WriteLine($"Открыта матрешка размера {size}...");
+    OpenMatreshka2(size-1);
+}
+OpenMatreshka2(5);
---
+++ Lesson5/Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Вычислить факториал от натурального числа N
+
+Console.Clear();
+int Fact(int n)
+{
+    if (n == 1 || n == 0)
+    {
+        Console.WriteLine($"STOP: {n}");
+        return 1;
+    }
+    int x = n;
+    Console.WriteLine(n);
+    n = n * Fact(n - 1);
+    Console.WriteLine($"Возврат: n = {x}, Fact = {n}");
+    return n;    
+}
+Console.Write(Fact(9));
