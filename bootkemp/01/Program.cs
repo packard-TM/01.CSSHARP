a0dec6e9e1891894348ed2741b73128decf8e085
packard_TM <a174126725@gmail.com>
2024-02-16 10:35:15 +0300

    добавлен буткемп и 2 урока

---
+++ bootkemp/01/Program.cs
@@ -0,0 +1,36 @@
+﻿// таблица умножения
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+for (int i = 1; i <= n; i++)
+{
+    for (int j = 1; j <= n; j++)
+    {
+        Console.Write($"{i * j}\t");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+
+
+int[,] matrix = new int[n, n];
+for (int i = 0; i < n; i++)
+{
+    for (int j = i; j < n; j++)
+    {
+        matrix[i, j] = (i + 1) * (j + 1);
+        matrix[j, i] = (i + 1) * (j + 1);
+    }
+    Console.WriteLine();
+}
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write(matrix[i, j]);
+        Console.Write("\t");
+    }
+    Console.WriteLine();
+}
---
+++ bootkemp/02/Program.cs
@@ -0,0 +1,48 @@
+﻿// void InputArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(0, 100);
+//     }
+// }
+
+// int[] sortVibor(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         int indexMin = i;
+//         for (int j = i; j < array.Length; j++)
+//         {
+//             if (array[j] < array[indexMin])
+//                 indexMin = j;
+//         }
+//         if (array[indexMin] == array[i])
+//             continue;
+
+//         int temp = array[i];
+//         array[i] = array[indexMin];
+//         array[indexMin] = temp;
+//     }
+//     return array;
+// }
+
+// Console.Clear();
+// Console.Write("Введите количество элементов массива: ");
+// int n = int.Parse(Console.ReadLine()!);
+// int[] array = new int[n];
+// InputArray(array);
+// Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+// Console.WriteLine($"Конечный массив: [{string.Join(", ", sortVibor(array))}]");
+
+using static Sorting;
+using static Infrastructure;
+
+// int[] array = CreateArray(10); // можно менять размер min и max (10, 100, 200)
+// Show(array);
+// SortSelection(array);
+// Show(array);
+
+10.CreateArray()
+.Show()
+.SortSelection()
+.Show();
