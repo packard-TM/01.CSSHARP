2e29190fed55ec5495f2cdfad26b0fa99a5044a0
packard_TM <a174126725@gmail.com>
2024-02-16 20:22:32 +0300

    добавлена пузырьковая сортировка (03)

---
+++ bootkemp/03/Program.cs
@@ -0,0 +1,35 @@
+﻿// пузырьковая сортировка
+
+int n = 20;
+int max = 100;
+int[] array = new int[n];
+
+bool Check(int[] array)
+{
+    int size = array.Length;
+    for (int i = 0; i < size - 1; i++)
+    {
+        if (array[i] > array[i + 1]) return false;
+    }
+
+    return true;
+}
+
+for (int i = 0; i < n; i++) array[i] = Random.Shared.Next(max);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+System.Console.WriteLine(Check(array));
+
+for (int k = 0; k < n - 1; k++)
+{
+    for (int i = 0; i < n - 1 - k; i++)
+    {
+        if (array[i] > array[i + 1])
+        {
+            int temp = array[i];
+            array[i] = array[i + 1];
+            array[i + 1] = temp;
+        }
+    }
+}
+Console.WriteLine($"[{String.Join(", ", array)}]");
+System.Console.WriteLine(Check(array));
